f023c213095790f22ae93c0e431099058228fdb5
Mauro Jotta <104471982+MauroJotta@users.noreply.github.com>
2025-07-27 11:46:36 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ window.misDatos = [
   { "categoria": "Noticias", "titulo": "TN", "url": "https://www.youtube.com/@todonoticias/live" },
   { "categoria": "Noticias", "titulo": "Telefe", "url": "https://tvlibreonline.org/html/dash.html?get=aHR0cHM6Ly90ZWxlZmV2aWFjb20xLmFrYW1haXplZC5uZXQvaGxzL2xpdmUvMjAzNzk4Ny92aWFjb21JTlQvVE9LL21hc3Rlci13aWxkMS5tM3U4" },
   { "categoria": "Noticias", "titulo": "Canal 13", "url": "https://tvlibreonline.org/html/hls.html?get=aHR0cHM6Ly9saXZlLTAxLTAyLWVsdHJlY2Uudm9kZ2MubmV0L2VsdHJlY2V0di9pbmRleC5tM3U4" },
-  { "categoria": "Noticias", "titulo": "TV Publica", "url": "https://tvlibreonline.org/html/fl/?get=Q2FuYWw3" },
+  { "categoria": "Noticias", "titulo": "TV Publica", "url": "https://streamtpglobal.com/global1.php?stream=tv_publica" },
   { "categoria": "Noticias", "titulo": "La Nacion", "url": "https://tvlibreonline.org/html/fl/?get=TGFfTmFjaW9u" },
   { "categoria": "Noticias", "titulo": "Canal Rural", "url": "https://tvlibreonline.org/html/fl/?get=Q2FuYWxfUnVyYWw=" },
   { "categoria": "Deportes", "titulo": "TyC", "url": "https://streamtpglobal.com/global1.php?stream=fox1ar#document%20(https://dlhd.click/embed/stream-375.php" },
